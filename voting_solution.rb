7f9a659d0031cf1f2f591c55f00b14796a07bcee
Dmitry Savitski <dmitry.savitsky@rightscale.com>
2016-03-29 11:45:42 +0300

    Add additional Sinatra actions

--- voting_solution.rb
+++ voting_solution.rb
@@ -3,7 +3,15 @@ class VotingSolution < Sinatra::Base
   set :public_folder => "public", :static => true
 
   get "/" do
+    erb :votes
+  end
+
+  get "/questions" do
     erb :questions
+  end
+
+  post "/vote" do
+    redirect '/'
   end
 
 end
